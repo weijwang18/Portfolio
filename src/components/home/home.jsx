8a75b8cf298127f0f045a3f712be4123a58b0b1e
Winnie Wang <weijwang18@gmail.com>
2023-01-06 03:59:10 -0800

    WIP: Updating Home Component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React from "react";
+import "./home.css";
+import Social from "./social";
+import Data from "./data";
 
 const Home = () => {
 	return (
-		<div>Home</div>
+		<section className="home section" id="home">
+			<div className="home_container container grid">
+				<div className="home_content grid">
+					<Social />
+					<div className="home_img"></div>
+					<Data />
+				</div>
+			</div>
+		</section>
 	)
 }
 
---
+++ src/components/home/social.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home_social">
+      <a href="https://github.com/weijwang18" className="home_social-icon" target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a href="https://www.linkedin.com/in/weijiewangseattle/" className="home_social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
