e0bb538e4c9fb015e31117c26a5561ff780ab42d
Winnie Wang <weijwang18@gmail.com>
2023-01-09 23:02:05 -0800

    update home component

--- src/components/home/social.jsx
+++ src/components/home/social.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Social = () => {
   return (
     <div className="home_social">
-      <a href="https://github.com/weijwang18" className="home_social-icon" target="_blank">
+      <a href="https://www.linkedin.com/in/weijiewangseattle/" className="home_social-icon" target="_blank">
         <i class="uil uil-linkedin-alt"></i>
       </a>
 
-      <a href="https://www.linkedin.com/in/weijiewangseattle/" className="home_social-icon" target="_blank">
+      <a href="https://github.com/weijwang18" className="home_social-icon" target="_blank">
         <i class="uil uil-github-alt"></i>
       </a>
     </div>
@@ -15,3 +15,6 @@ const Social = () => {
 };
 
 export default Social;
+
+
+
