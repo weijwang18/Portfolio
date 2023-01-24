67909c40f05fe3f2aada74cf5ba5206ae36b1f31
Winnie Wang <weijwang18@gmail.com>
2023-01-24 15:50:34 -0800

    update about component

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const Info = () => {
-	return (
-		<div></div>
-	)
-}
+  return (
+    <div className="about_info grid">
+      <div className="about_box">
+		<i class='bx bx-award about_icon'></i>
 
-export default Info
+        <h3 className="about_title">Experience</h3>
+        <h4 className="about_subtitle">May 2022 - Oct 2022</h4>
+        <span className="about_subtitle">27-week Bootcamp (Epicodus)</span>
+      </div>
+
+      <div className="about_box">
+		<i class='bx bx-briefcase-alt about_icon'></i>
+
+        <h3 className="about_title">Completed</h3>
+          <span className="about_subtitle">70+ Projects</span>
+      </div>
+
+      <div className="about_box">
+		<i class='bx bx-support about_icon'></i>
+		
+        <h3 className="about_title">Support</h3>
+          <span className="about_subtitle">Online 24/7</span>
+
+      </div>
+    </div>
+  );
+};
+
+export default Info;
